493bf95e15e34d24ad54abbd6b426b17104ec2a6
= <ethanmarkow@gmail.com>
2026-07-02 09:52:18 -0400

    changed checkIncorrect to pass by const reference rather than by value

--- src/code.cpp
+++ src/code.cpp
@@ -32,25 +32,26 @@ int const Code::checkCorrect(Code &guess){
     }
     return amountCorrect;
 };
-int const Code::checkIncorrect(Code guess) {
+int const Code::checkIncorrect(Code const &guess) {
     // Count the number of correct numbers in incorrect positions
     Code tempCode = *this; // copy of original code to modify
+    Code tempGuess = guess;
     int amountIncorrect = 0;
     // First replace all correct positions with -1 to indicate a correct guess
     for (int i = 0; i < n; i++)
     {
-        if (code[i] == guess.code[i]) {
+        if (code[i] == tempGuess.code[i]) {
             tempCode.code[i] = -1;
-            guess.code[i] = -1;
+            tempGuess.code[i] = -1;
         }
     }
     // Now check for incorrect positions, ignoring entries now marked as -1
     for (int i = 0; i < n; i++)
     {
-        if (guess.code[i] != -1)
+        if (tempGuess.code[i] != -1)
         {
             // See if there are any correct values in incorrect positions
-            auto it = std::find(tempCode.code.begin(), tempCode.code.end(), guess.code[i]);
+            auto it = std::find(tempCode.code.begin(), tempCode.code.end(), tempGuess.code[i]);
             // If find did find a matching value
             if (it != tempCode.code.end())
             {
@@ -59,7 +60,7 @@ int const Code::checkIncorrect(Code guess) {
                 // Replace value in tempCode with -1
                 *it = -1;
                 // Replace value in guess with -1
-                guess.code[i] = -1;
+                tempGuess.code[i] = -1;
             }
         }
     }
