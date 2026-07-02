05849a2ade7841f9d0907944811126237c289ca9
Emile <sater.e@northeastern.edu>
2026-07-02 14:42:19 -0400

    Added extensive comments to code.cpp and code.hpp files. No changes to main.cpp

--- src/code.cpp
+++ src/code.cpp
@@ -8,17 +8,18 @@
 
 
 #include "code.hpp"
-
+//initializer list. stores the integer values of n and m to the code object's member variables n and m (representing length and range)
 Code::Code(int n, int m) : n(n), m(m) {
-}
-Code::Code(int n, int m, vector<int> t) : n(n), m(m), code(t){
+} 
+//second constructor for when secret code is known. stores secret code to the code object's member variable along with n and m  
+Code::Code(int n, int m, vector<int> t) : n(n), m(m), code(t) {
 }
 
 // Generate a random code that is n digits long and is between 0 and m-1
-void Code::random() {
+void Code::random() { 
     //Randomly generate a number for each digit in the code and making it n length
-    for (int i = 0; i < n; i++) {
-        code.push_back(rand() % m);
+    for (int i = 0; i < n; i++) { //establishes code will be longer than 'n' length in for loop
+        code.push_back(rand() % m); //adds newly random generated integer to the end of the vector (growing it until i = n)
     }
     //print out the code for testing
     for (int i = 0; i < n; i++) {
@@ -26,22 +27,22 @@ void Code::random() {
     }
 };
 // Check how many digits are correct and in the correct position
-int const Code::checkCorrect(Code const &guess){
-    int amountCorrect = 0;
-    //check each digit in the code to see if it is the same as the guess in the same possition
-    for (int i = 0; i < n; i++) { 
+int const Code::checkCorrect(Code const& guess) { // initializes checkCorrect function of class 'code' that will return an int value of r1 (amount correct) using secret code and user guess
+    int amountCorrect = 0; //intializes default value for amount of numbers guessed correctly (0)
+    //check each digit in the code to see if it is the same as the guess in the same position
+    for (int i = 0; i < n; i++) {
         if (code[i] == guess.code[i]) {
-            amountCorrect += 1;
+            amountCorrect += 1; //if correct guess, add 1 to amountCorrect to imply correct choice    
         }
     }
     //return int value of how many are correct
     return amountCorrect;
 };
-int const Code::checkIncorrect(Code const &guess) {
+int const Code::checkIncorrect(Code const& guess) { //initializes checkInocorrect function of class 'code' that will return an int value of amount digits right but in wrong sequence
     // Count the number of correct numbers in incorrect positions
     Code tempCode = *this; // copy of original code to modify
-    Code tempGuess = guess;
-    int amountIncorrect = 0;
+    Code tempGuess = guess; //stores guess in tempGuess with class 'code' to compare with original class 'code' secret sequence 
+    int amountIncorrect = 0; 
     // First replace all correct positions with -1 to indicate a correct guess
     for (int i = 0; i < n; i++)
     {
@@ -55,14 +56,14 @@ int const Code::checkIncorrect(Code const &guess) {
     {
         if (tempGuess.code[i] != -1)
         {
-            // See if there are any correct values in incorrect positions
-            auto it = find(tempCode.code.begin(), tempCode.code.end(), tempGuess.code[i]);
+            // See if there are any correct values in incorrect positions using find() in order to seek out specific digit within sequence
+            auto it = std::find(tempCode.code.begin(), tempCode.code.end(), tempGuess.code[i]);
             // If find did find a matching value
             if (it != tempCode.code.end())
             {
                 amountIncorrect++;
                 // Ensure no double counts
-                // Replace value in tempCode with -1
+                // Replace value in tempCode with -1 (as it will not be a proper user input)
                 *it = -1;
                 // Replace value in guess with -1
                 tempGuess.code[i] = -1;
@@ -71,4 +72,3 @@ int const Code::checkIncorrect(Code const &guess) {
     }
     return amountIncorrect;
 };
-
--- src/code.hpp
+++ src/code.hpp
@@ -5,19 +5,30 @@
 //  Created by Jake Berman on 7/1/26.
 //
 
-#ifndef code_hpp
+//ensures header file is included only once
+#ifndef code_hpp //if not defined, then define (prevents redefinition of code_hpp)
 #define code_hpp
 
+<<<<<<< HEAD
 #include <stdio.h>
 #include <vector>
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
 #include <algorithm>
+=======
+#include <stdio.h> //includes C standard input/output library (I AM UNSURE IF WE NEED THIS (we have iostream). GO OVER TOGETHER)
+#include <vector> //includes vector container library
+#include <cstdlib> //includes functions such as rand() within library
+#include <ctime> //includes time() so we can use along with rand() for random number generator
+#include <iostream> //includes input/output streams
+#include <algorithm> //COME BACK TO THIS
+>>>>>>> 7446c28 (Added extensive comments to code.cpp and code.hpp files. No changes to main.cpp)
 
-using namespace std;
+using namespace std; //defines usage of std library globally
 
 
+<<<<<<< HEAD
 class Code {
     public:
     Code(int n, int m);
@@ -27,10 +38,21 @@ class Code {
     int const checkIncorrect(Code const &guess);
     
     private:
+=======
+class Code { //creating class 'code'
+public: //in a public section. these public member functions can be called outside of class
+    Code(int n, int m); //creates a new object that stores n and m (length and range)
+    Code(int n, int m, vector<int> t); //code object stores the already formed randomly generated secret code vector with length and range
+    void random(); //uses random function to generate the secret code
+    int const checkCorrect(Code const& guess); 
+    int const checkIncorrect(Code const& guess);
+
+private: //ensures n, m, and secret code can not be accessed by user and hidden in a private session.
+>>>>>>> 7446c28 (Added extensive comments to code.cpp and code.hpp files. No changes to main.cpp)
     int n;
     int m;
     vector<int> code;
-    
+
 };
 
 //the vector cant be private because we need to input the guess unless we add a function to do that 
