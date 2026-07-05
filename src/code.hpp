8681f0351f234096cf21cb5bce67e42b0d13e93f
Emile <sater.e@northeastern.edu>
2026-07-05 14:55:01 -0400

    Removed merge conflict remnants from earlier commit

--- src/code.hpp
+++ src/code.hpp
@@ -9,49 +9,33 @@
 #ifndef code_hpp //if not defined, then define (prevents redefinition of code_hpp)
 #define code_hpp
 
-<<<<<<< HEAD
 #include <stdio.h>
 #include <vector>
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
 #include <algorithm>
-=======
 #include <stdio.h> //includes C standard input/output library (I AM UNSURE IF WE NEED THIS (we have iostream). GO OVER TOGETHER)
 #include <vector> //includes vector container library
 #include <cstdlib> //includes functions such as rand() within library
 #include <ctime> //includes time() so we can use along with rand() for random number generator
 #include <iostream> //includes input/output streams
 #include <algorithm> //COME BACK TO THIS
->>>>>>> 7446c28 (Added extensive comments to code.cpp and code.hpp files. No changes to main.cpp)
 
 using namespace std; //defines usage of std library globally
-
-
-<<<<<<< HEAD
-class Code {
-    public:
-    Code(int n, int m);
-    Code(int n, int m, vector<int> t);
-    void random();
-    int const checkCorrect(Code const &guess);
-    int const checkIncorrect(Code const &guess);
     
-    private:
-=======
 class Code { //creating class 'code'
-public: //in a public section. these public member functions can be called outside of class
-    Code(int n, int m); //creates a new object that stores n and m (length and range)
-    Code(int n, int m, vector<int> t); //code object stores the already formed randomly generated secret code vector with length and range
-    void random(); //uses random function to generate the secret code
-    int const checkCorrect(Code const& guess); 
-    int const checkIncorrect(Code const& guess);
+    public: //in a public section. these public member functions can be called outside of class
+        Code(int n, int m); //creates a new object that stores n and m (length and range)
+        Code(int n, int m, vector<int> t); //code object stores the already formed randomly generated secret code vector with length and range
+        void random(); //uses random function to generate the secret code
+        int const checkCorrect(Code const& guess); 
+        int const checkIncorrect(Code const& guess);
 
-private: //ensures n, m, and secret code can not be accessed by user and hidden in a private session.
->>>>>>> 7446c28 (Added extensive comments to code.cpp and code.hpp files. No changes to main.cpp)
-    int n;
-    int m;
-    vector<int> code;
+    private: //ensures n, m, and secret code can not be accessed by user and hidden in a private session.
+        int n;
+        int m;
+        vector<int> code;
 
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,6 @@
 int main(int argc, const char * argv[]) {
     srand(time(NULL));
 
-<<<<<<< HEAD
-
     Code secretCode(5, 7);
     cout << "Secret Code: ";
     secretCode.random();
@@ -23,14 +21,12 @@ int main(int argc, const char * argv[]) {
     cout << endl << "Guess 1: " << secretCode.checkCorrect(guess1) << " " << secretCode.checkIncorrect(guess1) << endl;
     cout << "Guess 2: " << secretCode.checkCorrect(guess2) << " " << secretCode.checkIncorrect(guess2) << endl;
     cout << "Guess 3: " << secretCode.checkCorrect(guess3) << " " << secretCode.checkIncorrect(guess3) << endl;
-=======
     vector<int> t = {2, 1, 0, 2, 1}; 
     vector<int> x = {2, 2, 0, 1, 2};
     Code test(5, 3, t);
     Code guess(5, 3, x);
     //test.random();
     cout << test.checkCorrect(guess) << endl;
->>>>>>> 7446c28 (Added extensive comments to code.cpp and code.hpp files. No changes to main.cpp)
     return EXIT_SUCCESS;
 }
 
